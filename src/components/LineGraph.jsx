ec0ea06e23e0bd5072781f48be7256cf0c262303
Muhammad Sarim Khan Bangash <75114586+Muhammad-Sarim-Khan-Bangash@users.noreply.github.com>
2021-07-05 17:31:45 +0500

    label change

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -15,7 +15,7 @@ const LineGraph = (props) => {
           labels: props.label,
           datasets: [
             {
-              label: "My First dataset",
+              label: "COVID_19 Data",
               fill: false,
               lineTension: 0.1,
               backgroundColor: "rgba(75,192,192,0.4)",
